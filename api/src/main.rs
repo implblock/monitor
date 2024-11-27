bf4782d9a82c1f15350b3e2ce5b27520e7c71b7e
implblock <implblock@proton.me>
2024-11-27 11:48:10 -0600

    Setup the router

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,9 +1,31 @@
+use anyhow::Context;
+use axum::Router;
+use tokio::net::TcpListener;
+
 pub mod routes;
 
 // for simple empty results
 type Any = anyhow::Result<()>;
 
+const PORT: u16 = 6942;
+
 #[tokio::main]
 async fn main() -> Any {
+    let addr = std::env::var("ADDR").unwrap_or(
+        // the default shouldn't be loopback
+        format!("0.0.0.0:{}", PORT)
+    );
+
+    let socket = TcpListener::bind(addr).await
+        .with_context(|| "connecting to socket")?;
+
+    let router = Router::new();
+
+    axum::serve(
+        socket,
+        router,
+    ).await
+    .with_context(|| "serving")?;
+
     Ok(())
 }
