ac866b42800f5b5c8d8b83b5150c2550cc17fae8
implblock <implblock@proton.me>
2024-11-30 18:55:39 -0600

    Err

--- api/src/error.rs
+++ api/src/error.rs
@@ -1,21 +1,59 @@
 use axum::{http::StatusCode, response::IntoResponse};
 
-pub struct ApiError(StatusCode, anyhow::Error);
+use crate::resources::{cpu, memory, network, uptime};
+
+pub struct ApiError(anyhow::Error);
 
 impl<E> From<E> for ApiError
 where
     E: Into<anyhow::Error>,
 {
     fn from(value: E) -> Self {
-        ApiError(StatusCode::INTERNAL_SERVER_ERROR, value.into())
+        let err: anyhow::Error = value.into();
+
+        ApiError(err)
     }
 }
 
 impl IntoResponse for ApiError {
     fn into_response(self) -> axum::response::Response {
+        // TODO
+        // make better
+        if let Some(e) = self.0.downcast_ref::<cpu::UsageError>() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("{:#?}", e),
+            )
+            .into_response();
+        }
+
+        if let Some(e) = self.0.downcast_ref::<network::Error>() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("{:#?}", e),
+            )
+            .into_response();
+        }
+
+        if let Some(e) = self.0.downcast_ref::<uptime::Error>() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("{:#?}", e),
+            )
+            .into_response();
+        }
+
+        if let Some(e) = self.0.downcast_ref::<memory::Error>() {
+            return (
+                StatusCode::INTERNAL_SERVER_ERROR,
+                format!("{:#?}", e),
+            )
+            .into_response();
+        }
+
         (
-            self.0,
-            self.1.to_string(),
+            StatusCode::INTERNAL_SERVER_ERROR,
+            format!("{:#?}", self.0)
         )
         .into_response()
     }
